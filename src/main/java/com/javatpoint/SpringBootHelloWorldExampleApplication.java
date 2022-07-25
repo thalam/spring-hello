3c4899b4c9096ad948660bffd7e189845f797fd1
Jane Aarthy Joseph <jjose139@my.centennialcollege.ca>
2022-07-25 10:12:34 -0400

    First commit

--- src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -3,11 +3,13 @@ package com.javatpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringBootHelloWorldExampleApplication
-{
-public static void main(String[] args) 
-{
-SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
-}
+public class SpringBootHelloWorldExampleApplication {
+    public static void main(String[] args) {
+        com.example.serverA.demoA.DemoAApplication demoAApplication;
+        com.example.serverB.demoB.DemoBApplication demoBApplication;
+
+        SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+    }
 }
